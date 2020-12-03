74b58fb450843cbdd586bf3fa118ccad0df21ab8
Loïc <ldancelme@gmail.com>
2020-12-03 11:05:33 +0100

    + taln

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Author : LOX
-
+@author : LOX
 """
 
 import requests
 from bs4 import BeautifulSoup
-import urlencode
-word = 'bite'
+
+word = input()
 url = 'http://www.synonymo.fr/syno/' + word
 get = requests.get(url)
+get.encoding = get.apparent_encoding
 html = get.text
 
 
 
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, features='lxml')
 raw_text = soup.prettify()
 
 ul = soup.find('ul', {'class':'synos'})
@@ -23,6 +23,11 @@ first = ul.findAll('a')
 synos = []
 for i in first:
     synos.append(i.next)
+
+
+print('\nSynonyms of {} :'.format(word))
+for s in synos:
+    print(s)
 
 # # Write synonyms to a new file
 # file = word + '.txt'
